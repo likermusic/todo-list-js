f776f101f34d544041fb077ef46c99f9dddcfe2d
Sergey Timofeev <drumliker@yandex.ru>
2023-08-20 22:03:00 +0300

    Вывели задачи при загрузке страницы + поиск по id

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function renderItem(markup, outputClass) {
   document.querySelector(outputClass).insertAdjacentHTML('beforeend', markup);
 }
@@ -9,7 +7,7 @@ function clearInput(input) {
 }
 
 function addItemToLS(name, source, params = null) { // {important: false, done: false}
-  let items = JSON.parse(localStorage.getItem(source)) || [];
+  let items = getItemsFromLS(source);
   let newItem;
   if (items.length == 0) {
     newItem = { id: 1, name, ...params }; //{ id: 1, name:dfd, important: false, done: false } // { id: 1, name }
@@ -21,19 +19,59 @@ function addItemToLS(name, source, params = null) { // {important: false, done: 
   localStorage.setItem(source, JSON.stringify(items));
 }
 
+function getItemsFromLS(key) {
+  return JSON.parse(localStorage.getItem(key)) || [];
+}
 
 document.querySelector('.btn-add').addEventListener('click', function (event) {
   const input = event.target.parentElement.querySelector('input');
-  const li = `<li class="list-group-item d-flex justify-content-between">
+  const tasks = getItemsFromLS('tasks');
+  const id = tasks[tasks.length - 1].id + 1;
+  const li = `<li data-id="${id}" class="list-group-item d-flex justify-content-between">
   <span>${input.value}</span>
-  <div class="btn-actions">
-    <a class="btn-important text-dark" href=""><i class="fs-5 bi bi-exclamation-circle-fill me-1"></i></a>
-    <a class="btn-delete text-dark" href=""><i class="fs-5 bi bi-x-circle-fill"></i></a>
+  <div class="btn-actions"><a class="btn-important text-dark" href=""><i class="fs-5 bi bi-exclamation-circle-fill me-1"></i></a>
+  <a class="btn-delete text-dark" href=""><i class="fs-5 bi bi-x-circle-fill"></i></a>
   </div>
   </li>`;
   addItemToLS(input.value, 'tasks', { important: false, done: false });
   renderItem(li, '.list');
   clearInput(input)
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const tasks = getItemsFromLS('tasks');
+  tasks.forEach(function (item, ind) {
+    const li = `<li data-id=${item.id} class="list-group-item d-flex justify-content-between">
+  <span>${item.name}</span>
+  <div class="btn-actions"><a class="btn-important text-dark" href=""><i class="fs-5 bi bi-exclamation-circle-fill me-1"></i></a>
+  <a class="btn-delete text-dark" href=""><i class="fs-5 bi bi-x-circle-fill"></i></a>
+  </div>
+  </li>`;
+    renderItem(li, '.list');
+
+
+  })
+})
+
+
+
+
+document.querySelector('.list').addEventListener('click', function (e) {
+  if (e.target.matches('span')) {
+    e.target.classList.toggle('done');
+
+
+    const tasks = getItemsFromLS('tasks');
+    tasks.forEach(function (item, ind) {
+      if (e.target.parentElement.dataset.id == item.id) {
+        console.log(item);
+      }
+    });
+
+  }
+})
+
+
+
 
 
