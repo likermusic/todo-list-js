9626bc350a748a32686bde8470124118d2c7bce2
Sergey Timofeev <drumliker@yandex.ru>
2023-08-27 19:46:37 +0300

    Сделали состояние done

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ function clearInput(input) {
 function addItemToLS(name, source, params = null) { // {important: false, done: false}
   let items = getItemsFromLS(source);
   let newItem;
+  let id;
   if (items.length == 0) {
-    newItem = { id: 1, name, ...params }; //{ id: 1, name:dfd, important: false, done: false } // { id: 1, name }
+    id = 1;
+    newItem = { id: id, name, ...params }; //{ id: 1, name:dfd, important: false, done: false } // { id: 1, name }
   } else {
-    const id = items[items.length - 1].id + 1;
+    id = items[items.length - 1].id + 1;
     newItem = { id, name, ...params };
   }
   items.push(newItem);
   localStorage.setItem(source, JSON.stringify(items));
+  return id;
 }
 
 function getItemsFromLS(key) {
@@ -26,14 +29,13 @@ function getItemsFromLS(key) {
 document.querySelector('.btn-add').addEventListener('click', function (event) {
   const input = event.target.parentElement.querySelector('input');
   const tasks = getItemsFromLS('tasks');
-  const id = tasks[tasks.length - 1].id + 1;
+  const id = addItemToLS(input.value, 'tasks', { important: false, done: false });
   const li = `<li data-id="${id}" class="list-group-item d-flex justify-content-between">
   <span>${input.value}</span>
   <div class="btn-actions"><a class="btn-important text-dark" href=""><i class="fs-5 bi bi-exclamation-circle-fill me-1"></i></a>
   <a class="btn-delete text-dark" href=""><i class="fs-5 bi bi-x-circle-fill"></i></a>
   </div>
   </li>`;
-  addItemToLS(input.value, 'tasks', { important: false, done: false });
   renderItem(li, '.list');
   clearInput(input)
 });
@@ -41,15 +43,17 @@ document.querySelector('.btn-add').addEventListener('click', function (event) {
 document.addEventListener('DOMContentLoaded', function () {
   const tasks = getItemsFromLS('tasks');
   tasks.forEach(function (item, ind) {
+    // let clazz = '';
+    // if (item.done == true) {
+    //   clazz = 'done';
+    // }
     const li = `<li data-id=${item.id} class="list-group-item d-flex justify-content-between">
-  <span>${item.name}</span>
+  <span class="${item.done == true ? 'done' : ''}">${item.name}</span>
   <div class="btn-actions"><a class="btn-important text-dark" href=""><i class="fs-5 bi bi-exclamation-circle-fill me-1"></i></a>
   <a class="btn-delete text-dark" href=""><i class="fs-5 bi bi-x-circle-fill"></i></a>
   </div>
   </li>`;
     renderItem(li, '.list');
-
-
   })
 })
 
@@ -64,9 +68,11 @@ document.querySelector('.list').addEventListener('click', function (e) {
     const tasks = getItemsFromLS('tasks');
     tasks.forEach(function (item, ind) {
       if (e.target.parentElement.dataset.id == item.id) {
-        console.log(item);
+        item.done = !item.done;
       }
     });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
   }
 })
